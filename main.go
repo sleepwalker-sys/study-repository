986dc1e00f3d7c4839c2afed176fd127200ab715
SleepWalker <rinosoundslikeserino@gmail.com>
2026-01-12 19:54:18 +0300

    add feature 2

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"study/feature1"
+	"study/feature2"
 )
 
 func main() {
 	fmt.Println("Hello Git!")
 	feature1.Feature1()
+	feature2.Feature2()
 }
 
 // Перед тем как создавать свою репозиторию нужно дать понять нашему аккаунту на GitHub что мы можем спокойно сохранять проект,
